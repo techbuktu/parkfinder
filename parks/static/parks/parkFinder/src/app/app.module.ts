72293821973b7cd7770f97a37702074c76dc49f3
Muhammad Jalloh <techbuktu@jalloh.com>
2018-10-23 09:16:31 -0400

    Updated app.module.ts to reflect mv of routing module to app/modules/ folder

--- parks/static/parks/parkFinder/src/app/app.module.ts
+++ parks/static/parks/parkFinder/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from './app.component';
 import { ParkDetailComponent } from './components/park-detail/park-detail.component';
 import { ParkListComponent } from './components/park-list/park-list.component';
